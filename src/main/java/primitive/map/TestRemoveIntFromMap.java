f3d0fcac0fdb5abd63b03d85d4c2545439b7ac6e
Sergey Voronezhtsev <sergey.voronezhtsev@trafigura.com>
2017-01-16 17:04:54 +0300

    add test

--- src/main/java/primitive/map/TestRemoveIntFromMap.java
+++ src/main/java/primitive/map/TestRemoveIntFromMap.java
@@ -1,7 +1,20 @@
 package primitive.map;
 
+import com.carrotsearch.hppc.IntIntScatterMap;
+import com.koloboke.collect.map.hash.HashIntIntMap;
+import com.koloboke.collect.map.hash.HashIntIntMaps;
+import gnu.trove.map.TIntIntMap;
+import gnu.trove.map.hash.TIntIntHashMap;
+import it.unimi.dsi.fastutil.ints.Int2IntLinkedOpenHashMap;
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
+import org.eclipse.collections.impl.map.mutable.primitive.IntIntHashMap;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.AverageTime)
@@ -13,4 +26,122 @@ import java.util.concurrent.TimeUnit;
 public class TestRemoveIntFromMap {
     @Param({"30000"})
     private int size;
+
+    private Map<Integer,Integer> map1;
+    private Map<Integer,Integer> map2;
+    private Map<Integer,Integer> map3;
+    private Int2IntOpenHashMap map4;
+    private Int2IntLinkedOpenHashMap map5;
+    private com.carrotsearch.hppc.IntIntHashMap map6;
+    private IntIntScatterMap map7;
+    private HashIntIntMap map8;
+    private TIntIntMap map9;
+    private IntIntHashMap map10;
+
+    @Benchmark
+    public void test_Oracle_HashMap() {
+        int count = 0;
+        for(int i = 0; i < size; i+=3) {
+            count += map1.remove(i);
+        }
+    }
+
+    @Benchmark
+    public void test_Oracle_TreeMap() {
+        for(int i = 0; i < size; i+=3) {
+            map2.remove(i);
+        }
+    }
+
+    @Benchmark
+    public void test_Oracle_LinkedMap() {
+        for(int i = 0; i < size; i+=3) {
+            map3.remove(i);
+        }
+    }
+
+    @Benchmark
+    public void test_FastUtil_IntOpenHashMap() {
+        for(int i = 0; i < size; i+=3) {
+            map4.remove(i);
+        }
+    }
+
+    @Benchmark
+    public void test_FastUtil_IntLinkedOpenHashMap() {
+        for(int i = 0; i < size; i+=3) {
+            map5.remove(i);
+        }
+    }
+
+    @Benchmark
+    public void test_Hppc_IntIntHashMap() {
+        for(int i = 0; i < size; i+=3) {
+            map6.remove(i);
+        }
+    }
+
+    @Benchmark
+    public void test_Hppc_IntIntScatterMap() {
+        for(int i = 0; i < size; i+=3) {
+            map7.remove(i);
+        }
+    }
+
+    @Benchmark
+    public void test_Koloboke_HashIntIntMap() {
+        for(int i = 0; i < size; i+=3) {
+            map8.remove(i);
+        }
+    }
+
+    @Benchmark
+    public void test_Trove_TIntIntHashMap() {
+        for(int i = 0; i < size; i+=3) {
+            map9.remove(i);
+        }
+    }
+
+    @Benchmark
+    public void test_Eclipse_IntIntHashMap() {
+        for(int i = 0; i < size; i+=3) {
+            map10.remove(i);
+        }
+    }
+
+    @Setup(Level.Iteration)
+    public void tearDown() {
+        map1 = new HashMap<>(size);
+        map2 = new TreeMap<>();
+        map3 = new LinkedHashMap<>(size);
+        map4 = new Int2IntOpenHashMap(size);
+        map5 = new Int2IntLinkedOpenHashMap(size);
+        map6 = new com.carrotsearch.hppc.IntIntHashMap(size);
+        map7 = new IntIntScatterMap(size);
+        map8 = HashIntIntMaps.getDefaultFactory().newMutableMap(size);
+        map9 = new TIntIntHashMap(size);
+        map10 = new IntIntHashMap(size);
+
+        for(int i = 0; i < size; i++) {
+            map1.put(i,i);
+            map2.put(i,i);
+            map3.put(i,i);
+            map4.put(i,i);
+            map5.put(i,i);
+            map6.put(i,i);
+            map7.put(i,i);
+            map8.put(i,i);
+            map9.put(i,i);
+            map10.put(i,i);
+        }
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(TestRemoveIntFromMap.class.getSimpleName())
+                .param("size","30000","50000","100000","500000","1000000")
+                .build();
+
+        new Runner(opt).run();
+    }
 }
